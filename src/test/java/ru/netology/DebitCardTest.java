49af41889c0624773fae5db2e6b9c17948020192
Greg Poslov <grig.skamsk@mail.ru>
2023-02-25 13:23:32 +0500

    first commit

---
+++ src/test/java/ru/netology/DebitCardTest.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DebitCardTest {
+    @Test
+    void happyTest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[data-test-id=callback-form]");
+        $("[data-test-id=name] input").setValue("Тапков Василий");
+        $("[data-test-id=phone] input").setValue("+79823332211");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button__content")).click();
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+}
